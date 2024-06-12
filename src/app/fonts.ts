c351c20f82276b89c836ce9d3fe7212abf23931a
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2024-06-13 00:11:07 +0800

    feat: set up nav bar

---
+++ src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Lato, Inter } from "next/font/google";
+
+export const lato = Lato({
+  weight: ["700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
